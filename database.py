ec23598f0c577b722622de51282e5e843ac0b6b3
Angelo Martin Pertus <angelopertus16@gmail.com>
2023-06-29 14:07:54 -0300

    database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+import psycopg2 as bd
+
+conexion = bd.connect(
+    usuario= 'postgres',
+    password= 'metallida',
+    host= '190.138.33.20',
+    port= '192.168.1.0',
+    database= 'vacunatorio'
+
+)
