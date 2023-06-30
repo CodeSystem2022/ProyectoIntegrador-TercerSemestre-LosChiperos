91b8b596949af1f9817ce531a6c4df812d40c0da
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2023-06-30 00:38:57 -0300

    cambios

--- api.py
+++ api.py
@@ -1,21 +1,11 @@
-def editar_paciente(id):
-    nombre = request.form['nombre']
-    segundo_nombre = request.form['segundo_nombre']
-    apellido = request.form['apellido']
-    nro_dni = request.form['nro_dni']
-    fecha_nacimiento = request.form['fecha_nacimiento']
-    dosis = request.form['dosis']
-    centro_salud = request.form['centro_salud']
-    
+def borrar_paciente(id):
     try:
         with db:
             with db.cursor() as cursor:
-                sentencia = 'UPDATE paciente SET nombre=%s, segundo_nombre = %s, apellido=%s,nro_dni=%s, fecha_nacimiento=%s, dosis=%s, centro_salud=%s WHERE id_paciente=%s'
-                valores = (nombre, segundo_nombre, apellido,nro_dni,fecha_nacimiento,dosis,
-                        centro_salud, id)
-                cursor.execute(sentencia, valores)
+                sentencia = "DELETE FROM pacientes WHERE id = %s;"
+                cursor.execute(sentencia, (id,))
     except Exception as e:
-        print(f'No se pudo modificar los valores: {e}')
+        print('Error al borrar el paciente:  {e}')
     finally:
         cursor.close()
         return redirect(url_for('home'))
