be5d3b15d42baa8b335169a7c4493855acb01562
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2023-06-30 00:48:44 -0300

    correciones

--- app.py
+++ app.py
@@ -4,29 +4,28 @@ from api import home, agregar_paciente, borrar_paciente, editar_paciente, listar
 app = Flask(__name__)
 
 # Ruta para la página de inicio
-@app.route('/')
+app.route('/')
 
 
 # Ruta para agregar un paciente (POST)
-@app.route('/agregar_paciente', methods=['POST'])
+app.route('/agregar_paciente', methods=['POST'])
 
 
 # Ruta para borrar un paciente (DELETE)
-@app.route('/borrar_paciente/<int:id>', methods=['DELETE'])
+app.route('/borrar_paciente/<int:id>', methods=['DELETE'])
 
 
 # Ruta para editar un paciente (PUT)
-@app.route('/editar_paciente/<int:id>', methods=['PUT'])
+app.route('/editar_paciente/<int:id>', methods=['PUT'])
 
 
 # Ruta para listar el equipo (GET)
-@app.route('/listar_equipo', methods=['GET'])
+app.route('/listar_equipo', methods=['GET'])
 
 
 # Ruta para agregar un integrante al equipo (POST)
-@app.route('/agregar_integrante_equipo', methods=['POST'])
+app.route('/agregar_integrante_equipo', methods=['POST'])
 
 
 # Ruta para el inicio de sesión (POST)
-@app.route('/login', methods=['POST'])
-
+app.route('/login', methods=['POST'])
