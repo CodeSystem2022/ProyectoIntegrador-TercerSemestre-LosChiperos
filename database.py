3d4badea33fe9e817168acdac4c069e012528cb3
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2023-06-30 00:42:40 -0300

    correcion de databse

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ conexion = bd.connect(
     usuario= 'postgres',
     password= 'metallida',
     host= 'base-vacunarg.postgres.database.azure.com',
-    port= '5432'
+    port= '5432',
     database= 'vacunatorio'
 
 )
