49f15c613cccaea44ef2e0fea65d5cf143d3a689
Oscar Gonzalo Araya <gonxaraya991@gmail.com>
2023-06-30 15:36:15 -0300

    vaccineForm cargado

--- templates/javascript/api.js
+++ templates/javascript/api.js
@@ -1,19 +1,22 @@
 const apiUrl = "https://api.vacunarg.site/buscar_paciente";
 
+// Función para validar el número de CUIL utilizando una expresión regular
 function isValidCuil(cuil) {
     var re = /^([20|23|24|27]{2})-([0-9]{8})-([0-9]{1})$/;
     return re.test(cuil);
 }
 
+// Manejador de evento para el envío del formulario
 document.getElementById("vaccineForm").addEventListener("submit", function (event) {
-    event.preventDefault();
+    event.preventDefault(); // Prevenir el comportamiento predeterminado de enviar el formulario
 
     var cuil = document.getElementById("cuil").value;
     if (!isValidCuil(cuil)) {
-        document.getElementById("cuilError").classList.remove("hidden");
+        document.getElementById("cuilError").classList.remove("hidden"); // Mostrar mensaje de error en caso de CUIL inválido
         return;
     }
 
+    // Realizar una solicitud POST a la API
     fetch(apiUrl, {
         method: 'POST',
         headers: {
@@ -24,13 +27,14 @@ document.getElementById("vaccineForm").addEventListener("submit", function (even
         })
     })
         .then((response) => {
-            if (!response.ok) throw new Error(response.status);
-            else return response.json();
+            if (!response.ok) throw new Error(response.status); // Lanzar un error si la respuesta no es exitosa
+            else return response.json(); // Analizar los datos de la respuesta como JSON y devolverlos como una promesa
         })
         .then((data) => {
             console.log(data);
 
             if (data.length === 0) {
+                // Mostrar un mensaje de error en caso de no encontrar datos para el CUIL proporcionado
                 Swal.fire({
                     title: 'Error!',
                     text: 'No se encontraron datos para el CUIL proporcionado.',
@@ -42,6 +46,7 @@ document.getElementById("vaccineForm").addEventListener("submit", function (even
 
             let paciente = data[0];
 
+            // Formatear las fechas de nacimiento y aplicación en un formato legible
             let fechaNacimiento = new Date(paciente.fecha_nacimiento);
             let formattedFechaNacimiento = fechaNacimiento.getDate() + '/' + (fechaNacimiento.getMonth() + 1) +
                 '/' + fechaNacimiento.getFullYear();
@@ -50,6 +55,7 @@ document.getElementById("vaccineForm").addEventListener("submit", function (even
             let formattedFechaAplicacion = fechaAplicacion.getDate() + '/' + (fechaAplicacion.getMonth() + 1) +
                 '/' + fechaAplicacion.getFullYear();
 
+            // Mostrar un mensaje de éxito con los datos del paciente
             Swal.fire({
                 title: 'Éxito!',
                 html: 'Nombre: ' + paciente.nombre + '<br>' +
@@ -60,22 +66,23 @@ document.getElementById("vaccineForm").addEventListener("submit", function (even
                     'Fecha de Aplicación: ' + formattedFechaAplicacion + '<br>' +
                     'Centro de Salud: ' + paciente.centro_salud + '<br>' +
                     'Nombre de Vacuna: ' + paciente.nombre_vacuna + '<br>' +
-                    'Lote de Vacuna: ' + paciente.lote_vacuna.replace(/\n/g,
-                        '<br>'), // Reemplaza '\n' con '<br>' para saltos de línea
+                    'Lote de Vacuna: ' + paciente.lote_vacuna.replace(/\n/g, '<br>'), // Reemplaza '\n' con '<br>' para saltos de línea
                 icon: 'success',
                 confirmButtonText: 'Ok'
             });
 
         })
         .catch((error) => {
-            console.log(error);
+            console.log(error); // Mostrar cualquier error en la consola
         });
 });
 
+// Evento que se dispara cuando se carga el contenido de la página
 document.addEventListener("DOMContentLoaded", function () {
     const counters = document.querySelectorAll('.counter');
     const speed = 200; // Cuánto tiempo te gustaría que dure la animación
 
+    // Observador de intersección para activar la animación cuando el elemento esté en la vista
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
@@ -97,10 +104,10 @@ document.addEventListener("DOMContentLoaded", function () {
                     updateCount();
                 });
 
-                observer.disconnect();
+                observer.disconnect(); // Detener la observación después de activar la animación
             }
         });
     });
 
-    observer.observe(document.querySelector('section.bg-cover'));
+    observer.observe(document.querySelector('section.bg-cover')); // Observar el elemento que activará la animación
 });
